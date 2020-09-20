1c031b7d3fa188c35d30958a31357104d7e75537
xiaohui <2698401299>
2020-09-20 12:13:57 +0800

    完成user创建 get 和 set,杀一个斌斌祭天!!

--- UserCloudProject/user-register/src/main/java/cn/ekgc/user/UserRegisterStarter.java
+++ UserCloudProject/user-register/src/main/java/cn/ekgc/user/UserRegisterStarter.java
@@ -1,6 +1,8 @@
 package cn.ekgc.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * <b>注册中心启动器</b>
@@ -8,6 +10,8 @@ import org.springframework.boot.SpringApplication;
  * @version 4.1.0
  * @since 4.1.0
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class UserRegisterStarter {
 	public static void main(String[] args) {
 		SpringApplication.run(UserRegisterStarter.class, args);
