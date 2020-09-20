29292dd1d5c13792e5e7a7223f158a2cbe6225ae
xiaohui <2698401299>
2020-09-20 11:07:54 +0800

    完成注册中心创建，用一个斌斌祭天！！！

---
+++ UserCloudProject/user-register/src/main/java/cn/ekgc/user/UserRegisterStarter.java
@@ -0,0 +1,15 @@
+package cn.ekgc.user;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * <b>注册中心启动器</b>
+ * @author xiaohui
+ * @version 4.1.0
+ * @since 4.1.0
+ */
+public class UserRegisterStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegisterStarter.class, args);
+	}
+}
